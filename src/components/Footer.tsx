730c18987c60b8d62164944f12bc6b7b4da37f82
Peter <petersolomon704@gmail.com>
2023-06-17 20:59:37 +0100

    footer
    modified code

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Footer: React.FC = () => {
+  return <MainWrap>hello</MainWrap>;
+};
+
+const MainWrap = styled.div``;
+
+export default Footer;
